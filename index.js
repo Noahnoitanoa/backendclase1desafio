03b66d1c9a71b05b7be3b8123e9459abed43a9b6
Noah Gil <gilpereznoah@gmail.com>
2022-10-15 20:39:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function probar(){
+    const nombre= 'noah'
+
+let saludo = `hola, 
+soy ${nombre}`
+let edad = 40
+
+console.log(nombre)
+console.log(edad)
+console.log(saludo)
+
+edad += 5
+
+console.log(edad)
+
+const diccionario = {
+    color: 'rojo',
+    cuidad: 'rio'
+}
+
+// const array = {
+//     'o': 'mariano',
+//     '1': 'willa',
+//     '2': 'laura'
+
+// }
+
+const array = [
+    mariano,
+     will,
+    laura,
+
+]
+
+console.log(array)
+console.log(array['o'])
+
+}
+
+probar()
